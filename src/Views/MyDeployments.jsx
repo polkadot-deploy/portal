5f7075d54070d471e1f6b35a46aecd699683e5d9
Javier Viola <152718445+olaf-at-work@users.noreply.github.com>
2024-09-05 00:40:21 +0200

    interval

--- src/Views/MyDeployments.jsx
+++ src/Views/MyDeployments.jsx
@@ -8,7 +8,7 @@ import { DeploymentCard } from '../Components/DeploymentCard';
 export const MyDeployments = () => {
     const { selectedAccount } = useContext(AccountsContext)
     const [userDeploymentsList, setUserDeployments] = useState([])
-    
+
     useEffect(() => {
         const getuserDeployments = async () => {
             const _userDeploymentsList = await userDeployments(selectedAccount.address);
@@ -18,6 +18,11 @@ export const MyDeployments = () => {
         if(!userDeploymentsList.length && selectedAccount){
             getuserDeployments()
         }
+
+        const intervalId = setInterval(() => {
+            getuserDeployments()
+          }, 1000 * 2) // in milliseconds
+          return () => clearInterval(intervalId)
     },[selectedAccount])
 
     return (
@@ -25,12 +30,12 @@ export const MyDeployments = () => {
         <div className="max-w-7xl mx-auto">
             {
                 !selectedAccount ? <h1 className="text-3xl font-bold mb-6 bg-gradient-to-r from-[#E6007A] to-[#6D3AEE] text-transparent bg-clip-text">Please connect Wallet</h1>
-                : !userDeploymentsList.length ? <h1 className="text-3xl font-bold mb-6 bg-gradient-to-r from-[#E6007A] to-[#6D3AEE] text-transparent bg-clip-text">No Deployment history</h1> 
+                : !userDeploymentsList.length ? <h1 className="text-3xl font-bold mb-6 bg-gradient-to-r from-[#E6007A] to-[#6D3AEE] text-transparent bg-clip-text">No Deployment history</h1>
                 :
                     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
                         {userDeploymentsList.length && userDeploymentsList.map((deployment) => (
                             <DeploymentCard key={deployment.ext_id} {...deployment} />
-                        ))}    
+                        ))}
                     </div>
             }
       </div>
