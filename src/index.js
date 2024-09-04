eecdaa8390aa62f42014dcdb00cf0364714aebc4
Santiago Balaguer <santiago.balaguer@gmail.com>
2024-09-04 12:35:04 +0200

    removed some unnecesary code for now

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //Context Imports
-import { ApiConnect } from './Context/ApiConnect'
-import { RelayData } from './Context/RelayData'
+// import { ApiConnect } from './Context/ApiConnect'
+// import { RelayData } from './Context/RelayData'
 import { Accounts } from './Context/Accounts'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApiConnect>
+    {/* <ApiConnect> */}
       <Accounts>
-        <RelayData>
+        {/* <RelayData> */}
           <App />
-        </RelayData>
+        {/* </RelayData> */}
       </Accounts>
-    </ApiConnect>
+    {/* </ApiConnect> */}
   </React.StrictMode>
 );
 
