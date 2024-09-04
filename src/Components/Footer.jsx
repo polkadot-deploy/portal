f67c4ba23f2c0c65dc98d8f1846c323c5563edf7
Santiago Balaguer <santiago.balaguer@gmail.com>
2024-09-04 16:28:32 +0200

    changed based-path + others

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,11 +4,11 @@ export const Footer = () => {
     return (
         <footer className="bg-white mt-8 py-4 text-center text-sm text-gray-500 shadow-sm">
             <div className="max-w-7xl mx-auto px-4">
-            <div>Version: 3e8d2...05113</div>
-            <div className="space-x-4">
+            <div>MVP v0.1 - Made by PDP Team</div>
+            {/* <div className="space-x-4">
                 <a href="#" className="hover:text-gray-700">Terms of Service</a>
                 <a href="#" className="hover:text-gray-700">Privacy Policy</a>
-            </div>
+            </div> */}
             </div>
         </footer>
     )
